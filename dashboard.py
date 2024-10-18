58e7d95b599c7f247909e17257833d2b2204b452
James Twose <james.twose2711@gmail.com>
2024-10-18 23:32:43 +0200

    getting the LM studio stuff working with example data in a streamlit dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,27 +1,63 @@
-from pandasai import Agent
-from pandasai.llm.local_llm import LocalLLM
 import pandas as pd
 import streamlit as st
+import requests
+import plotly.express as px
+import plotly.graph_objects as go
 
-ollama_llm = LocalLLM(api_base="http://localhost:11434/v1", model="codellama")
-sales_by_country = pd.DataFrame(
-    {
-        "country": [
-            "United States",
-            "United Kingdom",
-            "France",
-            "Germany",
-            "Italy",
-            "Spain",
-            "Canada",
-            "Australia",
-            "Japan",
-            "China",
+
+@st.cache_data(show_spinner="Getting Data", ttl=3600)
+def get_data():
+    sales_by_country = pd.DataFrame(
+        {
+            "country": [
+                "United States",
+                "United Kingdom",
+                "France",
+                "Germany",
+                "Italy",
+                "Spain",
+                "Canada",
+                "Australia",
+                "Japan",
+                "China",
+            ],
+            "sales": [
+                5000,
+                3200,
+                2900,
+                4100,
+                2300,
+                2100,
+                2500,
+                2600,
+                4500,
+                7000,
+            ],
+        }
+    )
+
+    return sales_by_country
+
+
+@st.cache_data(show_spinner="Awaiting response from model", ttl=3600)
+def get_chat_response(
+    data: pd.DataFrame, question_for_agent: str
+):
+    chat_prompt = f"Based on the sales data below, {question_for_agent}\n\n{data}"
+    new_body = {
+        "messages": [
+            {"role": "user", "content": "Put yourself in the shoes of a data scientist"},
+            {"role": "user", "content": chat_prompt},
         ],
-        "sales": [5000, 3200, 2900, 4100, 2300, 2100, 2500, 2600, 4500, 7000],
+        "model": "",
     }
-)
-agent = Agent(sales_by_country, config={"llm": ollama_llm})
+
+    response = requests.post(
+        "http://localhost:1234/v1/chat/completions",
+        json=new_body,
+    )
+    return response.json().get("choices")[0].get("message").get("content")
+
 
 # configure the page
 st.set_page_config(
@@ -31,12 +67,31 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
+df = get_data()
+
 st.title("Dashboard")
-st.dataframe(sales_by_country)
-question_for_agent = st.text_input("Ask a question based on the data", "Which are the bottom 5 countries by sales?")
+st.write("This is a dashboard for the sales data")
+st.dataframe(df.T)
+question_for_agent = st.text_input(
+    "Ask a question based on the data",
+    "Which are the bottom 5 countries by sales?",
+)
+
 if st.button("Ask"):
     print(question_for_agent)
     st.write(question_for_agent)
-    result = agent.chat(question_for_agent)
-    print(result)
-    st.write(result)
+    result = get_chat_response(
+        data=df,
+        question_for_agent=question_for_agent,
+    )
+    st.code(result)
+
+code_to_execute = st.text_input("Enter code to execute", "")
+if st.button("Execute"):
+    print(code_to_execute)
+    if "fig = " in code_to_execute:
+        exec(code_to_execute)
+        st.plotly_chart(fig)
+    else:
+        exec(f"result={code_to_execute}")
+        st.write(result)
